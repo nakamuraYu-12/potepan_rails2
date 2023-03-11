5fb7c24d7384f3fcc10323ed2fa27621b04345ed
NakamuraY <nakamuray.pg@gmail.com>
2023-03-11 19:50:33 +0900

    roomモデル、コントローラー作成

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,4 @@
 class TopController < ApplicationController
   def Index
-    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_10_195255) do
+ActiveRecord::Schema.define(version: 2023_03_11_103613) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.string "introduction"
+    t.integer "price"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
